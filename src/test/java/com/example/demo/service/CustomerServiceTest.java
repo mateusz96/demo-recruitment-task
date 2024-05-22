9536b0df62a0ec6f1bbf1b0291b8908cd2217412
Mateusz Domagalski <mati@192.168.0.100>
2024-05-22 16:50:50 +0200

    Add tests for pesel uniquness

--- src/test/java/com/example/demo/service/CustomerServiceTest.java
+++ src/test/java/com/example/demo/service/CustomerServiceTest.java
@@ -5,13 +5,14 @@ import static com.example.demo.common.TestFixtures.aCustomerWithBirthDate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import com.example.demo.exceptions.BusinessException;
 import com.example.demo.exceptions.ResourceNotFoundException;
 import com.example.demo.persistence.model.Customer;
 import com.example.demo.persistence.repository.CustomerRepository;
+import com.example.demo.util.SqlExceptionHandler;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceTest {
@@ -44,6 +46,19 @@ public class CustomerServiceTest {
         customerService.save(customer);
 
         verify(customerRepository).save(customer);
+    }
+
+    @Test
+    public void shouldThrowException_whenSavingCustomerWithSamePesel() {
+        var uniqueKey = "customers_pesel_key";
+        var customer = aCustomer();
+        Throwable sqlException = new SQLException(uniqueKey, "23505");
+        Exception exception = new DataIntegrityViolationException(uniqueKey, sqlException);
+        doThrow(exception).when(customerRepository).save(customer);
+
+        assertThatThrownBy(() -> customerService.save(customer))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage(SqlExceptionHandler.PESEL_UNIQUE_MESSAGE);
     }
 
     @Test
